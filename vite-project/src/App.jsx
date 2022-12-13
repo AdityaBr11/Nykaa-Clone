418425fb5eb1280b41cd6e4300286d7b46da55a2
AdityaBr11 <adityaanandpurnea1@gmail.com>
2022-12-13 14:54:19 +0530

    temp

---
+++ vite-project/src/App.jsx
@@ -0,0 +1,14 @@
+
+import './App.css'
+import Homepage from './Pages/Homepage'
+
+function App() {
+
+  return (
+    <div>
+      <Homepage/>
+    </div>
+  )
+}
+
+export default App
---
+++ vite-project/src/Pages/Homepage.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Homepage = () => {
+  return (
+    <div className='text-7xl'>
+      Homepage
+    </div>
+  )
+}
+
+export default Homepage
