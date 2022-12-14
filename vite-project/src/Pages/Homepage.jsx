acabea9cd4ed519952ed4916ed9377dff1a7cc39
Santosh <san.kr.sharma090@gmail.com>
2022-12-14 18:33:24 +0530

    day-2 work done

--- vite-project/src/Pages/Homepage.jsx
+++ vite-project/src/Pages/Homepage.jsx
@@ -1,11 +1,25 @@
-import React from 'react'
+import React from "react";
+import {
+  Featured,
+  Focus,
+  Gamezone,
+  OnlyatNaykaa,
+  Popupstores,
+  TopBrand,
+} from "../Components/TopBrand";
 
 const Homepage = () => {
   return (
-    <div className='text-7xl'>
-      Homepage
+    <div className="text-7xl">
+      {/* Homepage */}
+      <TopBrand />
+      <OnlyatNaykaa />
+      <Featured />
+      <Focus />
+      <Gamezone />
+      <Popupstores />
     </div>
-  )
-}
+  );
+};
 
-export default Homepage
+export default Homepage;
